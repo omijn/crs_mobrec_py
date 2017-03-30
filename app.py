6d2d1d7a7e6994016996fbda9a5ea1e15c258822
omijn <victory_kashyap@yahoo.com>
2017-03-30 04:41:24 -0400

    extract noun phrases

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import request
 
 # nlp libs
-from stat_parser import Parser
+# from stat_parser import Parser
 import nltk
 
 # others
@@ -26,31 +26,23 @@ def handler():
     resolvedQuery = reqBody['result']['resolvedQuery']
     
     # NLTK POS tagging 
-#     print("POS Tagging: List of Tuples")
-    posTags = nltk.pos_tag(nltk.word_tokenize(resolvedQuery))
-    print(posTags)
-    print(type(posTags))
-    for tag in posTags: print(str(tag)) # or pp.pprint(posTags)
+    posSentence = nltk.pos_tag(nltk.word_tokenize(resolvedQuery))
     
-    # NP-chunking using a tag pattern
+    # NP-chunking using a tag pattern - refer to NLTK book chapter 7 for more details
     grammar = """NP: {<DT>?<RB.*>*<JJ.*>*<CD>?<NN.*>+}
         PP: {<IN><NP>}
         NP: {<NP><PP>}
         """
     rp = nltk.RegexpParser(grammar)
-    result = rp.parse(posTags)
-    print(type(str(result)))
-    print(result)
-       
-    # pyStatParser phrase structure parsing
-#     print("\nParse Tree: ")
-#     parseTree = parser.parse(resolvedQuery)
-#     print(type(parseTree))
-#     print(parseTree)
+    result = rp.parse(posSentence)
     
-    # create api.ai response 
-    speech = str(result)
-    response = {"speech": speech, "displayText": speech, "data": {}, "contextOut": [], "source": "GSMArena"}
+    # extract noun phrases from tree
+    returnText = "Important phrases\n"
+    for subtree in result.subtrees():
+        if subtree.label() == "NP": returnText += (str(subtree) + "\n")      
+    
+    # create api.ai response     
+    response = {"speech": returnText, "displayText": returnText, "data": {}, "contextOut": [], "source": "GSMArena"}
 
     # a response HAS to be sent
     return json.dumps(response)
